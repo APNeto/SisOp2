4e82d0ed0dd848484a873095f6976b005a7d404d
APNeto <penaneto@gmail.com>
2018-10-06 23:19:28 -0300

    Parcialidade da função upload

--- client.cpp
+++ client.cpp
@@ -10,48 +10,70 @@
 #include <pthread.h>
 #include <iostream>
 #include <string>
+#include <fstream>
 
-int uploadfile(){
-	std::cout<<"Em uploadfile"<< std::endl;
+#define MAXPAYLOAD 1500 - 4 * sizeof(uint16_t)
 
-//	n = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
-//	if (n < 0) 
-//		printf("ERROR sendto");
-//	
-//	length = sizeof(struct sockaddr_in);
-//	n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &from, &length);
-//	if (n < 0)
-//		printf("ERROR recvfrom");
-//
-//	printf("Got an ack: %s\n", buffer);
+int uploadfile()
+{
+	std::cout << "Em uploadfile" << std::endl;
+
+	//	n = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
+	//	if (n < 0)
+	//		printf("ERROR sendto");
+	//
+	//	length = sizeof(struct sockaddr_in);
+	//	n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &from, &length);
+	//	if (n < 0)
+	//		printf("ERROR recvfrom");
+	//
+	//	printf("Got an ack: %s\n", buffer);
 
 	return 0;
 }
 
-int downloadfile(){
-	std::cout<<"Em downloadfile"<< std::endl;
+int downloadfile()
+{
+	std::cout << "Em downloadfile" << std::endl;
 	return 0;
-	}
-int deletefile(){return 0;}
-int list_server(){return 0;}
-int list_client(){return 0;}
-int get_sync_dir(){return 0;}
-int exit(){return 0;}
+}
+int deletefile()
+{
+	std::cout << "Em deletefile" << std::endl;
+	return 0;
+}
+int list_server()
+{
+	std::cout << "Em lis_server" << std::endl;
+	return 0;
+}
+int list_client()
+{
+	std::cout << "Em list_client" << std::endl;
+	return 0;
+}
+int get_sync_dir()
+{
+	std::cout << "Em get_sync_dir" << std::endl;
+	return 0;
+}
+int exit() { return 0; }
 
 #define MAXNAME 256
 
 char username[MAXNAME];
-char path_to_syncdir[MAXNAME*2];
+char path_to_syncdir[MAXNAME * 2];
 
 int main(int argc, char *argv[])
 {
-    if(argc < 4){
-        std::cout << "Argumentos fornecidos nao correspondentes. Formato aceito: \n" <<
-            "./myClient <username> <server_ip_address> <port>\n";
-        exit(1);
-    }
+	if (argc < 4)
+	{
+		std::cout << "Argumentos fornecidos nao correspondentes. Formato aceito: \n"
+				  << "./myClient <username> <server_ip_address> <port>\n";
+		exit(1);
+	}
 
-    int sockfd, n;
+	int sockfd, n;
 	unsigned int length;
 	struct sockaddr_in serv_addr, from;
 	struct hostent *server;
@@ -59,15 +81,16 @@ int main(int argc, char *argv[])
 	uint16_t PORT;
 
 	strcpy(username, argv[1]);
-    strcpy(hostname, argv[2]);
-    PORT = atoi(argv[3]);
-	std::cout << PORT<<"\n";
-	char buffer[256];
+	strcpy(hostname, argv[2]);
+	PORT = atoi(argv[3]);
+	std::cout << PORT << "\n";
+	char buffer[MAXPAYLOAD];
 	server = gethostbyname(argv[2]);
-	if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
+	if (server == NULL)
+	{
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
 
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 		printf("ERROR opening socket");
@@ -76,46 +99,97 @@ int main(int argc, char *argv[])
 	serv_addr.sin_port = htons(PORT);
 	serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
 	bzero(&(serv_addr.sin_zero), 8);
-	
-	printf("Enter the message:\n");
-	bzero(buffer, 256);
-	fgets(buffer, 256, stdin);
 
-	n = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
-	if (n < 0) 
-		printf("ERROR sendto");
-	
-	length = sizeof(struct sockaddr_in);
-	n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &from, &length);
-	if (n < 0)
-		printf("ERROR recvfrom");
+	std::string comand;
 
-	printf("Got an ack: %s\n", buffer);
+	while (1)
+	{
+		std::cin.clear();
+		std::getline(std::cin, comand);
+		if (comand == "upload")
+		{
+			std::ifstream myfile ("teste.txt", std::ifstream::in);
+			int i = 0;
+			char *memblock;
+			double size;
+			if (myfile.is_open())
+			{
+				myfile.seekg(0, std::ios::end);
+				auto filesize = (double)myfile.tellg();
+				myfile.seekg(0, std::ios::beg);
+				std::cout << filesize;
+				int n;
+				int ab = (int)filesize % MAXPAYLOAD;
+				while (filesize > MAXPAYLOAD)
+				{
+					myfile.read(buffer, MAXPAYLOAD);
+					std::cout << "\nREAD:" << strlen(buffer) << "\n"
+							  << "SENT:";
+					n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr_in));
+					
+					if (n < 0)  printf("ERROR sendto");
+					printf("Got an ack: %s\n", buffer);
 
-//	while(1){
-//		if(comand == "upload"){
-//			uploadfile(); continue;
-//		}
-//		if(comand == "download"){
-//			downloadfile(); continue;
-//		}
-//		if(comand == "delete"){
-//			deletefile(); continue;
-//		}
-//		if(comand == "list_client"){
-//			list_client(); continue;
-//		}
-//		if(comand == "list_server"){
-//			list_server(); continue;
-//		}
-//		if(comand == "get_sync_dir"){
-//			get_sync_dir(); continue;
-//		}
-//		if(comand == "exit"){
-//			exit(); continue;
-//		}
-//		std::cout << "Comando invalido\n";
-//	}
+					filesize = filesize - MAXPAYLOAD;
+					std::cout << n << "\n";
+				}
+				myfile.read(buffer, MAXPAYLOAD);
+				n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr_in));
+				std::cout << "HI" << std::endl;
+			}
+			else
+			{
+				printf("\nFILE NOT OPENED \n");
+			}
+			//				printf("Enter the message:\n");
+			//				bzero(buffer, 256);
+			//				fgets(buffer, 256, stdin);
+			//				n = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
+			//				if (n < 0)
+			//					printf("ERROR sendto");
+			//
+			//				length = sizeof(struct sockaddr_in);
+			//				n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &from, &length);
+			//				if (n < 0)
+			//					printf("ERROR recvfrom");
+			//
+			//				printf("Got an ack: %s\n", buffer);
+			//				//uploadfile();
+
+			continue;
+		}
+		if (comand == "download")
+		{
+			downloadfile();
+			continue;
+		}
+		if (comand == "delete")
+		{
+			deletefile();
+			continue;
+		}
+		if (comand == "list_client")
+		{
+			list_client();
+			continue;
+		}
+		if (comand == "list_server")
+		{
+			list_server();
+			continue;
+		}
+		if (comand == "get_sync_dir")
+		{
+			get_sync_dir();
+			continue;
+		}
+		if (comand == "exit")
+		{
+			exit();
+			continue;
+		}
+		std::cout << "Comando invalido\n";
+	}
 
 	close(sockfd);
 	return 0;
--- server.cpp
+++ server.cpp
@@ -6,15 +6,17 @@
 #include <string.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <packet.h>
 
 #define PORT 4000
+#define MAXPAYLOAD 1500-4*sizeof(uint16_t)
 
 int main(int argc, char *argv[])
 {
 	int sockfd, n;
 	socklen_t clilen;
 	struct sockaddr_in serv_addr, cli_addr;
-	char buf[256];
+	char buf[MAXPAYLOAD];
 		
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) 
 		printf("ERROR opening socket");
@@ -41,6 +43,29 @@ int main(int argc, char *argv[])
 		if (n  < 0) 
 			printf("ERROR on sendto");
 	}
+
+/************** Codigo para Upload para o servidor
+//	while (1) {
+//		/* receive from socket */
+//		n = recvfrom(sockfd, buf, MAXPAYLOAD, 0, (struct sockaddr *) &cli_addr, &clilen);
+//		if (n < 0) 
+//			printf("ERROR on recvfrom");
+//		printf("Received a datagram: %s\n", buf);
+//		
+//		Ve se arquivo existe
+//		FILE *fp;
+// 		fp=fopen(buff,"r");
+//  		if(fp==NULL)
+//   		{
+//    		printf("file does not exist\n");
+//    	}
+//
+//		
+//		/* send to socket */
+//		n = sendto(sockfd, "Got your message\n", 17, 0,(struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
+//		if (n  < 0) 
+//			printf("ERROR on sendto");
+//	}
 	
 	close(sockfd);
 	return 0;
