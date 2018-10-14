cf2b2df53973969b1ef899d5d758f318ab08f859
APNeto <penaneto@gmail.com>
2018-10-14 18:45:23 -0300

    Corrigingo bugs em main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 void handle_user_input()
 {
-    cout << endl << "$ ";
+    std::cout << std::endl << "$ ";
 
     std::string line, cmd, arg;
 
-    while (getline(cin, line)) {
+    while (getline(std::cin, line)) {
         std::istringstream iss(line);
         iss >> cmd;
 
@@ -15,50 +30,44 @@ void handle_user_input()
                 download_file(arg);
             }
             else {
-                cout << "Command syntax: `download <filename>`" << endl;
+                std::cout << "Command syntax: `download <filename>`" << std::endl;
             }
         }
-
         else if (cmd == "upload") {
             iss >> arg;
 
             if (arg.length() > 0) {
                 queue->set_message(Message::MSG_UPDT_FILE, arg);
-                cout << "File `" << arg << "` added to upload queue." << endl;
+                std::cout << "File `" << arg << "` added to upload queue." << std::endl;
             }
                         else {
-                cout << "Command syntax: `upload /path/to/file`" << endl;
+                std::cout << "Command syntax: `upload /path/to/file`" << std::endl;
             }
         }
-
         else if (cmd == "delete") {
             iss >> arg;
 
             if (arg.length() > 0) {
                 queue->set_message(Message::MSG_DELE_FILE, arg);
-                cout << "File `" << arg << "` added to deletion queue." << endl;
+                std::cout << "File `" << arg << "` added to deletion queue." << std::endl;
             }
             else {
-                cout << "Command syntax: `delete <filename>`" << endl;
+                std::cout << "Command syntax: `delete <filename>`" << std::endl;
             }
         }
-
         else if (cmd == "list_client") {
             list_client();
         }
         else if (cmd == "list_server") {
             list_server();
         }
-
         else if (cmd == "exit") {
             close_connection();
             exit(EXIT_SUCCESS);
         }
-
         else {
-            cout << "Unknown command." << endl;
+            std::cout << "Unknown command." << std::endl;
         }
-
-        cout << "$ ";
+        std::cout << "$ ";
     }
 }
